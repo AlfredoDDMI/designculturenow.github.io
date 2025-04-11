58c92db00c664b2f590636817bd6bad1a464f8df
AlfredoDDMI <al241941@alumnos.uacj.mx>
2025-04-11 10:00:19 -0600

    Delete js directory

--- js/boletos.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('boton-comprar').addEventListener('click', function() {
-    const tipoBoleto = document.getElementById('tipo-boleto').value;
-    const cantidadBoletos = document.getElementById('cantidad-boletos').value;
-    // Aquí puedes agregar la lógica para procesar la compra
-    alert(`Comprando ${cantidadBoletos} boletos ${tipoBoleto}`);
-});
